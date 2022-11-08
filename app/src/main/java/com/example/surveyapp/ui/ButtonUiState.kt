66c52babded92cd2f58d0aba9fba286a4c9285b5
mz185 <marinoszenonos@gmail.com>
2022-11-08 23:05:00 +0200

    Create ButtonUiState

---
+++ app/src/main/java/com/example/surveyapp/ui/ButtonUiState.kt
@@ -0,0 +1,10 @@
+package com.example.surveyapp.ui
+
+/**
+ * Created by Marinos Zinonos on 08/11/2022.
+ */
+
+data class ButtonUiState(
+    val text: String = "",
+    val isEnabled: Boolean = false
+)
